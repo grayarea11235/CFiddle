430ff25b2eac065fbe89d816fd79ce4c077fe8f3
cpd <ciaran.dunn@gmail.com>
2019-09-19 13:09:15 +0100

    Adding read_xml

--- read_xml.c
+++ read_xml.c
@@ -12,11 +12,11 @@ parseStory (xmlDocPtr doc, xmlNodePtr cur)
   cur = cur->xmlChildrenNode;
   while (cur != NULL) {
     if ((!xmlStrcmp(cur->name, (const xmlChar *)"keyword")))
-      {
-	key = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
-	printf("keyword: %s\n", key);
-	xmlFree(key);
-      }
+    {
+      key = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
+      printf("keyword: %s\n", key);
+      xmlFree(key);
+    }
     cur = cur->next;
   }
 
@@ -32,37 +32,37 @@ parseDoc(char *docname)
   doc = xmlParseFile(docname);
   
   if(doc == NULL)
-    {
-      fprintf(stderr,"Document not parsed successfully. \n");
-      return;
-    }
+  {
+    fprintf(stderr,"Document not parsed successfully. \n");
+    return;
+  }
   
   cur = xmlDocGetRootElement(doc);
   
   if (cur == NULL)
-    {
-      fprintf(stderr,"empty document\n");
-      xmlFreeDoc(doc);
-      return;
-    }
+  {
+    fprintf(stderr,"empty document\n");
+    xmlFreeDoc(doc);
+    return;
+  }
   
   if (xmlStrcmp(cur->name, (const xmlChar *) "story"))
-    {
-      fprintf(stderr,"document of the wrong type, root node != story");
-      xmlFreeDoc(doc);
-      return;
-    }
+  {
+    fprintf(stderr,"document of the wrong type, root node != story");
+    xmlFreeDoc(doc);
+    return;
+  }
   
   cur = cur->xmlChildrenNode;
   while (cur != NULL)
+  {
+    if((!xmlStrcmp(cur->name, (const xmlChar *)"storyinfo")))
     {
-      if((!xmlStrcmp(cur->name, (const xmlChar *)"storyinfo")))
-	{
-	  parseStory (doc, cur);
-	}
-      
-      cur = cur->next;
+      parseStory (doc, cur);
     }
+      
+    cur = cur->next;
+  }
   
   xmlFreeDoc(doc);
   return;
@@ -74,10 +74,10 @@ main(int argc, char **argv)
   char *docname;
 		
   if (argc <= 1)
-    {
-      printf("Usage: %s docname\n", argv[0]);
-      return(0);
-    }
+  {
+    printf("Usage: %s docname\n", argv[0]);
+    return(0);
+  }
 
   docname = argv[1];
   parseDoc(docname);
