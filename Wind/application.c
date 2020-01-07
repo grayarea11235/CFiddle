443855b53532c62f29171cba17787a7ef458e182
grayarea11235 <ciaran.dunn@gmail.com>
2020-01-07 19:04:19 +0000

    More stuff

---
+++ Wind/application.c
@@ -0,0 +1,23 @@
+#include <windows.h>
+
+#include "application.h"
+
+//void add_form_to_application(application_t *app, form_t *form)
+
+application_t *create_application(HINSTANCE hInstance,
+                                  HINSTANCE hPrevInstance,
+                                  PSTR szCmdLine,
+                                  int iCmdShow)
+{
+  application_t *new_app = malloc(sizeof(application_t));
+
+  new_app->hInstance = hInstance;
+  new_app->hPrevInstance = hPrevInstance;
+  new_app->szCmdLine = szCmdLine;
+  new_app->iCmdShow = iCmdShow;
+  
+  return new_app;
+}
+
+
+
---
+++ Wind/application.h
@@ -0,0 +1,12 @@
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+#include "wind.h"
+
+application_t *create_application(HINSTANCE hInstance,
+                                  HINSTANCE hPrevInstance,
+                                  PSTR szCmdLine,
+                                  int iCmdShow);
+
+
+#endif
