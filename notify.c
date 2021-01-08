c0bbc0339774e60afe054912e3cd0b2a21fd8f79
cpd <ciaran.dunn@gmail.com>
2021-01-08 14:50:28 +0000

    Add notify.c

---
+++ notify.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <libnotify/notify.h>
+
+void print_usage(char *name)
+{
+  printf("Usage : %s <title> <message>\n", name);
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc != 3)
+  {
+    print_usage(argv[0]);
+    return 0;
+  }
+
+  notify_init("Hello world!");
+
+  // Stuff to add:
+  //
+  // Timeout
+  // Hint
+  // Urgency?
+  // Icon
+  // Custom icon
+  // Actions
+
+  
+  //  NotifyNotification * Hello = notify_notification_new("Hello world", "This is an example notification.", "dialog-information");
+  NotifyNotification *Hello = notify_notification_new(argv[1], argv[2], "dialog-information");
+
+  notify_notification_show (Hello, NULL);
+  g_object_unref(G_OBJECT(Hello));
+
+  notify_uninit();
+
+  return 0;
+}
