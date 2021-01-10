f8213bbc678d1c864c6a8c7aadecd5d3fdf94c72
grayarea11235 <ciaran.dunn@gmail.com>
2021-01-10 21:11:56 +0000

    Added dbus_example.c

--- c_dbus1.c
+++ c_dbus1.c
@@ -1,1 +1,29 @@
-#include <>
+
+   DBusError err;
+   DBusConnection* conn;
+   int ret;
+   // initialise the errors
+   dbus_error_init(&err);
+
+// connect to the bus
+   conn = dbus_bus_get(DBUS_BUS_SESSION, &err);
+   if (dbus_error_is_set(&err)) { 
+      fprintf(stderr, "Connection Error (%s)\n", err.message); 
+      dbus_error_free(&err); 
+   }
+   if (NULL == conn) { 
+      exit(1); 
+   }
+
+// request a name on the bus
+   ret = dbus_bus_request_name(conn, "test.method.server", 
+         DBUS_NAME_FLAG_REPLACE_EXISTING 
+         , &err);
+   if (dbus_error_is_set(&err)) { 
+      fprintf(stderr, "Name Error (%s)\n", err.message); 
+      dbus_error_free(&err); 
+   }
+   if (DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER != ret) { 
+      exit(1);
+   }
+
