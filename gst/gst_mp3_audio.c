4617ceb886aa48ec08e009cfc5ca99ca64f11cb8
cpd <ciaran.dunn@gmail.com>
2021-01-21 16:11:06 +0000

    tuff

--- gst/gst_mp3_audio.c
+++ gst/gst_mp3_audio.c
@@ -75,16 +75,17 @@ main (int   argc,
 
   /* Check input arguments */
   if (argc != 2) {
-    g_printerr ("Usage: %s <Ogg/Vorbis filename>\n", argv[0]);
+    g_printerr ("Usage: %s <mp3 filename>\n", argv[0]);
     return -1;
   }
 
-
+printf("1\n");
   /* Create gstreamer elements */
   pipeline = gst_pipeline_new ("audio-player");
   source   = gst_element_factory_make ("filesrc",       "file-source");
-  demuxer  = gst_element_factory_make ("oggdemux",      "ogg-demuxer");
-  decoder  = gst_element_factory_make ("vorbisdec",     "vorbis-decoder");
+//  demuxer  = gst_element_factory_make ("oggdemux",      "ogg-demuxer");
+//  decoder  = gst_element_factory_make ("vorbisdec",     "vorbis-decoder");
+  decoder  = gst_element_factory_make ("flump3dec",     "fluendo-decoder");
   conv     = gst_element_factory_make ("audioconvert",  "converter");
   sink     = gst_element_factory_make ("autoaudiosink", "audio-output");
 
@@ -92,6 +93,7 @@ main (int   argc,
     g_printerr ("One element could not be created. Exiting.\n");
     return -1;
   }
+printf("2\n");
 
   /* Set up the pipeline */
 
@@ -103,16 +105,24 @@ main (int   argc,
   bus_watch_id = gst_bus_add_watch (bus, bus_call, loop);
   gst_object_unref (bus);
 
+printf("3\n");
+
   /* we add all elements into the pipeline */
   /* file-source | ogg-demuxer | vorbis-decoder | converter | alsa-output */
   gst_bin_add_many (GST_BIN (pipeline),
                     source, demuxer, decoder, conv, sink, NULL);
+printf("3.1\n");
 
   /* we link the elements together */
   /* file-source -> ogg-demuxer ~> vorbis-decoder -> converter -> alsa-output */
   gst_element_link (source, demuxer);
   gst_element_link_many (decoder, conv, sink, NULL);
+
+printf("3.2\n");
+
   g_signal_connect (demuxer, "pad-added", G_CALLBACK (on_pad_added), decoder);
+
+printf("4\n");
 
   /* note that the demuxer will be linked to the decoder dynamically.
      The reason is that Ogg may contain various streams (for example
