4f060d86bf81d3bbe0816b26ad22e8416fb19fa7
grayarea11235 <ciaran.dunn@gmail.com>
2021-01-25 01:59:05 +0000

    Stuff

--- gst/gst_mp3_ui.c
+++ gst/gst_mp3_ui.c
@@ -1,14 +1,34 @@
 #include <gtk/gtk.h>
 
-static void
-activate (GtkApplication* app,
-          gpointer        user_data)
+static void btn_clk(GtkWidget *widget,
+    gpointer   data)
+{
+  g_print ("Button push\n");
+}
+
+static void activate (GtkApplication* app,
+    gpointer        user_data)
 {
   GtkWidget *window;
+  GtkWidget *button;
+  GtkWidget *button_box;
+  GtkWidget *list_box;
 
   window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_window_set_title(GTK_WINDOW (window), "Window");
+  gtk_window_set_default_size(GTK_WINDOW (window), 800, 600);
+
+  button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
+  gtk_container_add(GTK_CONTAINER(window), button_box);
+
+  button = gtk_button_new_with_label("Hello World");
+  g_signal_connect(button, "clicked", G_CALLBACK(btn_clk), NULL);
+  //g_signal_connect_swapped(button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
+
+  list_box = gtk_list_box_new();
+  gtk_container_add(GTK_CONTAINER (button_box), button);
+  gtk_container_add(GTK_CONTAINER (button_box), list_box);
+
   gtk_widget_show_all (window);
 }
 
