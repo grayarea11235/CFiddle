79c29da38715f20680bfa51cfe877edde9fdccb0
grayarea11235 <ciaran.dunn@gmail.com>
2021-01-26 02:39:09 +0000

    Stuff

--- gst/gst_mp3_ui.c
+++ gst/gst_mp3_ui.c
@@ -2,10 +2,18 @@
 #include <gst/gst.h>
 
 
+typedef struct _stream_info
+{
+  GMainLoop *loop;
+  GstElement *pipeline;
+  guint bus_watch_id;
+} stream_info;
+
+
 // Temp globals
-GMainLoop *loop;
-GstElement *pipeline;
-guint bus_watch_id;
+//GMainLoop *loop;
+//GstElement *pipeline;
+//guint bus_watch_id;
 
 static void gst_cleanup();
 
@@ -14,14 +22,15 @@ static gboolean bus_call(GstBus *bus,
     GstMessage *msg,
     gpointer data)
 {
-  GMainLoop *loop = (GMainLoop *) data;
-
+  //GMainLoop *loop = (GMainLoop *) data;
+  stream_info *stream_data = (stream_info *) data;
 
   switch (GST_MESSAGE_TYPE (msg)) 
   {
     case GST_MESSAGE_EOS:
       g_print ("End of stream\n");
-      gst_cleanup();
+      gst_cleanup(data);
+      g_free(data);
       break;
 
     case GST_MESSAGE_STREAM_START:
@@ -58,23 +67,20 @@ static void on_pad_added(GstElement *element,
 
   /* We can now link this pad with the vorbis-decoder sink pad */
   g_print ("Dynamic pad created, linking demuxer/decoder\n");
-
   sinkpad = gst_element_get_static_pad (decoder, "sink");
-
   gst_pad_link (pad, sinkpad);
-
   gst_object_unref (sinkpad);
 }
 
-static void gst_cleanup()
+static void gst_cleanup(stream_info *data)
 {
   g_print("In gst_cleanup()\n");
 
 
-  gst_element_set_state(pipeline, GST_STATE_NULL);
+  gst_element_set_state(data->pipeline, GST_STATE_NULL);
   g_print("Deleting pipeline\n");
-  gst_object_unref(GST_OBJECT (pipeline));
-  g_source_remove(bus_watch_id);
+  gst_object_unref(GST_OBJECT(data->pipeline));
+  g_source_remove(data->bus_watch_id);
   //g_main_loop_unref(loop);
 }
 
@@ -88,19 +94,20 @@ static void gst_start(char *filename)
              *conv, 
              *sink;
   GstBus *bus;
+  stream_info *data;
 
-
+  data = g_new(stream_info, 1);
 
   printf("1\n");
 
   /* Create gstreamer elements */
-  pipeline = gst_pipeline_new("audio-player");
+  data->pipeline = gst_pipeline_new("audio-player");
   source = gst_element_factory_make("filesrc", "file-source");
   decoder = gst_element_factory_make("flump3dec", "fluendo-decoder");
   conv = gst_element_factory_make("audioconvert", "converter");
   sink = gst_element_factory_make("autoaudiosink", "audio-output");
 
-  if (!pipeline || !source || !decoder || !conv || !sink) {
+  if (!data->pipeline || !source || !decoder || !conv || !sink) {
     g_printerr ("One element could not be created. Exiting.\n");
     printf("decoder = %p\n", decoder);
     return -1;
@@ -113,15 +120,15 @@ static void gst_start(char *filename)
   g_object_set(G_OBJECT (source), "location", filename, NULL);
 
   /* we add a message handler */
-  bus = gst_pipeline_get_bus(GST_PIPELINE (pipeline));
-  bus_watch_id = gst_bus_add_watch(bus, bus_call, loop);
+  bus = gst_pipeline_get_bus(GST_PIPELINE(data->pipeline));
+  data->bus_watch_id = gst_bus_add_watch(bus, bus_call, data);
   gst_object_unref(bus);
 
   printf("3\n");
 
   /* we add all elements into the pipeline */
   /* file-source | vorbis-decoder | converter | alsa-output */
-  gst_bin_add_many(GST_BIN (pipeline),
+  gst_bin_add_many(GST_BIN(data->pipeline),
       source, decoder, conv, sink, NULL);
   printf("3.1\n");
 
@@ -138,7 +145,7 @@ static void gst_start(char *filename)
 
   // Set the pipeline to "playing" state
   g_print ("Now playing: %s\n", filename);
-  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+  gst_element_set_state(data->pipeline, GST_STATE_PLAYING);
 
   /* Iterate */
   g_print ("Running...\n");
