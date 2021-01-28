ada0f3cccd42912ae75c254cc570b021e10acf65
grayarea11235 <ciaran.dunn@gmail.com>
2021-01-28 19:06:56 +0000

    Stuff

--- gst/file_util.c
+++ gst/file_util.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-typedef _file_info
+
+typedef struct _file_info
 {
   char *file_name;
   long size;
@@ -9,15 +14,30 @@ typedef _file_info
 
 void scan_dir(char *path)
 {
+  // /home/cpd/Downloads/Serhii\ Plokhy/Chernobyl\ -\ \ History\ of\ a\ Tragedy/
+  // /home/cpd/Downloads/Serhii Plokhy/Chernobyl -  History of a Tragedy
   DIR *dp;
   struct dirent *ep;     
-  dp = opendir("./");
+  struct stat statbuf;
+
+  dp = opendir("/home/cpd/Downloads/Serhii Plokhy/Chernobyl -  History of a Tragedy");
 
   if (dp != NULL)
   {
-    while(ep = readdir (dp))
+    while((ep = readdir(dp)) != 0)
     {
-      puts(ep->d_name);
+      printf("FILE=%s\n", ep->d_name); 
+
+      if (stat(ep->d_name, &statbuf) != -1)
+      {
+        printf("Filename = \"%s\" st_size = %ld\n", 
+            ep->d_name, 
+            statbuf.st_size);
+      }
+      else
+      {
+        perror("Error on stat!");
+      }
     }
 
     closedir(dp);
