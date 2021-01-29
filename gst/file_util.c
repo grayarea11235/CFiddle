4fbd52188172ea8f1bfa8a37d1b03383cfb54463
grayarea11235 <ciaran.dunn@gmail.com>
2021-01-29 14:13:40 +0000

    Stuff

--- gst/file_util.c
+++ gst/file_util.c
@@ -3,7 +3,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <apr.h>
 
 typedef struct _file_info
 {
@@ -20,24 +22,38 @@ void scan_dir(char *path)
   struct dirent *ep;     
   struct stat statbuf;
 
-  dp = opendir("/home/cpd/Downloads/Serhii Plokhy/Chernobyl -  History of a Tragedy");
+  dp = opendir(path);
 
   if (dp != NULL)
   {
     while((ep = readdir(dp)) != 0)
     {
-      printf("FILE=%s\n", ep->d_name); 
+      char *full_path = malloc(strlen(path) + strlen(ep->d_name) + 2);
+      strcpy(full_path, path);
+      strcat(full_path, "/");
+      strcat(full_path, ep->d_name);
+      printf("full_path = %s ", full_path);
 
-      if (stat(ep->d_name, &statbuf) != -1)
+
+      apr_status_t res;
+      res = apr_filepath_merge(char **  	newpath,
+          path,
+          ep->d_name,
+          apr_int32_t  	flags,
+          apr_pool_t *  	p);
+
+      if (stat(full_path, &statbuf) != -1)
       {
-        printf("Filename = \"%s\" st_size = %ld\n", 
-            ep->d_name, 
+        printf("st_size = %ld\n", 
             statbuf.st_size);
       }
       else
       {
-        perror("Error on stat!");
+        //perror("Error on stat!");
+        printf("Error on stat!\n");
       }
+
+      free(full_path);
     }
 
     closedir(dp);
@@ -50,5 +66,5 @@ void scan_dir(char *path)
 
 int main(void)
 {
-  scan_dir(NULL);
+  scan_dir("/home/cpd/Downloads/Serhii Plokhy/Chernobyl -  History of a Tragedy/");
 }
