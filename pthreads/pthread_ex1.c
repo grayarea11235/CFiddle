27a7d08ac412d034355d52afde4afd1611d1eb72
grayarea11235 <ciaran.dunn@gmail.com>
2021-01-31 00:00:30 +0000

    Stuff

--- pthreads/pthread_ex1.c
+++ pthreads/pthread_ex1.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <pthread.h>
 
-
 void *thread_func(void *ptr)
 {
-  printf("Hi from the thread"\n);
+  printf("Chickens\n");
 
   return NULL;
 }
 
-
 int main(int argc, char *argv[])
 {
-  if (pthread_create(&thread_func, NULL, thread_func, NULL))
+  pthread_t the_thread;
+
+  if (pthread_create(&the_thread, NULL, thread_func, NULL))
   {
-    printf("Thread created\n");
+    printf("Thread creation failed!\n");
   }
 
+  if(pthread_join(the_thread, NULL)) 
+  {
+    fprintf(stderr, "Error joining thread\n");
+    return 2;
+  }
+  else
+  {
+    printf("Thread joined\n");
+  }
 
   return 0;
 }
