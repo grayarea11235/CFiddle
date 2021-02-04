fd66a22c37bbb8cfa663945ef59761583824fcda
cpd <ciaran.dunn@gmail.com>
2021-02-04 23:20:08 +0000

    Stuff

--- sqlite/scan_audio_dir.c
+++ sqlite/scan_audio_dir.c
@@ -25,6 +25,15 @@ int scan_audio_dir(const char *directory)
 int main(int argc, char *argv[])
 {
 //  scan_audio_dir("/home/cpd/Downloads/Ursula K. Le Guin/Hainish Cycle/01 - The Dispossessed");
-  scan_audio_dir("/home/cpd/Downloads/Dennis E. Taylor - 2020 - Bobiverse, Book 4 - Heaven's River (Sci-Fi)/");
+//  scan_audio_dir("/home/cpd/Downloads/Dennis E. Taylor - 2020 - Bobiverse, Book 4 - Heaven's River (Sci-Fi)/");
+
+  if (argc != 2)
+  {
+    g_print("Usage : %s <filename/uri>\n", argv[0]);
+  }
+
+  // /home/cpd/Downloads/earthsea/1.1\ -\ Shadow.mp3 
+  scan_audio_dir(argv[1]);
+
   return EXIT_SUCCESS;
 }
