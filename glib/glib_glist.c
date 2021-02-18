9c81b9895db8f792eef2c8639dd3344f216874ee
cpd <ciaran.dunn@gmail.com>
2021-02-18 16:08:02 +0000

    Blrk

--- glib/glib_glist.c
+++ glib/glib_glist.c
@@ -1,11 +1,12 @@
 #include <glib.h>
+#include <stdio.h>
 
 int main (void)
 {
   GList *test_list = NULL;
 
-  test_list = g_list_append(list, "Hello world!");
-  printf("The first item is '%s'\n", g_list_first(list)‑>data);
+  test_list = g_list_append(test_list, "Hello world!");
+  printf("The first item is \"%s\"\n", (char *)g_list_first(test_list)->data);
 
   return EXIT_SUCCESS;
 }
