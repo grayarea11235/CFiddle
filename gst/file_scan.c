5fbb40e661a8868373d6be68f99ffb3081ead0e7
cpd <ciaran.dunn@gmail.com>
2021-10-22 15:42:54 +0100

    Scannign the files

--- gst/file_scan.c
+++ gst/file_scan.c
@@ -1,7 +1,7 @@
 // Scan a directory for audio files
 //
 // Build this with:
-// gcc -I/usr/include/taglib ./file_scan.c -o file_scan -ltag_c -ltag
+// gcc -I/usr/include/taglib ./file_scan.c -o file_scan -ltag_c -ltag -lsqlite3
 
 #include <stdio.h>
 #include <dirent.h>
@@ -15,6 +15,84 @@
 
 #include <tag_c.h>
 
+#define SQLITE_CALL(X)							\
+  {									\
+  int rc = sqlite3_##X;							\
+  if (rc != SQLITE_OK)							\
+  { \
+fprintf(stderr, "Call to %s failed. Statis %d : %s\n", #X, rc, sqlite3_errmsg(db)); \
+sqlite3_close(db); \
+exit(1);								\
+}									\
+}									\
+
+
+struct tag_info
+{
+  char *title;
+  char *artist;
+  char *album;
+  int year;
+  char *comment;
+  int track;
+  char *genre;
+};
+
+void dump_tag_info(struct tag_info *taginfo)
+{
+  printf("TAG INFO\n");
+  printf("  title   = %s\n", taginfo->title);
+  printf("  artist  = %s\n", taginfo->artist);
+  printf("  album   = %s\n", taginfo->album);
+  printf("  year    = %d\n", taginfo->year);
+  printf("  comment = %s\n", taginfo->comment);
+  printf("  track   = %d\n", taginfo->track);
+  printf("  genre   = %s\n", taginfo->genre);  
+}
+
+
+/*
+CREATE TABLE "tracks" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"title"	TEXT,
+	"artist"	TEXT,
+	"album"	TEXT,
+	"year"	INTEGER,
+	"comment"	TEXT,
+	"track"	INTEGER,
+	"genre"	TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+*/
+void add_track_record(struct tag_info *taginfo)
+{
+  sqlite3 *db;
+  sqlite3_stmt *res;
+
+  SQLITE_CALL(open("/home/cpd/Devel/CFiddle/gst/tracks.db", &db));
+  
+  char *sql = "INSERT INTO tracks VALUES(?, ?, ?, ?, ?, ?, ?, ?);";
+  sqlite3_stmt *stmt;
+  
+  SQLITE_CALL(prepare_v2(db, sql, strlen(sql) + 1, &stmt, NULL));
+    
+  SQLITE_CALL(bind_null(stmt, 1));
+  SQLITE_CALL(bind_text(stmt, 2, taginfo->title, strlen(taginfo->title),
+			0 /* The callback. */));
+  SQLITE_CALL(bind_text(stmt, 3, taginfo->artist, strlen(taginfo->artist),
+			0 /* The callback. */));
+  SQLITE_CALL(bind_text(stmt, 4, taginfo->album, strlen(taginfo->album),
+			0 /* The callback. */));
+  SQLITE_CALL(bind_int(stmt, 5, taginfo->year));
+  SQLITE_CALL(bind_text(stmt, 6, taginfo->comment, strlen(taginfo->comment),
+			0 /* The callback. */));
+  SQLITE_CALL(bind_int(stmt, 7, taginfo->track));
+  SQLITE_CALL(bind_text(stmt, 8, taginfo->genre, strlen(taginfo->genre),
+			0 /* The callback. */));
+
+
+  int s = sqlite3_step(stmt);
+}
 
 void file_info(const char *full_name)
 {
@@ -22,8 +100,8 @@ void file_info(const char *full_name)
   TagLib_Tag *tag;
   const TagLib_AudioProperties *properties;
 
-  printf("full_name=%s\n", full_name);
-  
+//  printf("full_name=%s\n", full_name);
+   
   file = taglib_file_new(full_name);
 
   if (file == NULL)
@@ -37,14 +115,27 @@ void file_info(const char *full_name)
 
   if(tag != NULL)
   {
-    printf("-- TAG --\n");
-    printf("title   - \"%s\"\n", taglib_tag_title(tag));
-    printf("artist  - \"%s\"\n", taglib_tag_artist(tag));
-    printf("album   - \"%s\"\n", taglib_tag_album(tag));
-    printf("year    - \"%i\"\n", taglib_tag_year(tag));
-    printf("comment - \"%s\"\n", taglib_tag_comment(tag));
-    printf("track   - \"%i\"\n", taglib_tag_track(tag));
-    printf("genre   - \"%s\"\n", taglib_tag_genre(tag));
+    struct tag_info taginfo;
+    taginfo.title = strdup(taglib_tag_title(tag));
+    taginfo.artist = strdup(taglib_tag_artist(tag));
+    taginfo.album = strdup(taglib_tag_album(tag));
+    taginfo.year = taglib_tag_year(tag);
+    taginfo.comment = strdup(taglib_tag_comment(tag));
+    taginfo.track = taglib_tag_track(tag);
+    taginfo.genre = strdup(taglib_tag_genre(tag));
+
+//    dump_tag_info(&taginfo);
+
+    add_track_record(&taginfo);
+    
+    /* printf("-- TAG --\n"); */
+    /* printf("title   - \"%s\"\n", taglib_tag_title(tag)); */
+    /* printf("artist  - \"%s\"\n", taglib_tag_artist(tag)); */
+    /* printf("album   - \"%s\"\n", taglib_tag_album(tag)); */
+    /* printf("year    - \"%i\"\n", taglib_tag_year(tag)); */
+    /* printf("comment - \"%s\"\n", taglib_tag_comment(tag)); */
+    /* printf("track   - \"%i\"\n", taglib_tag_track(tag)); */
+    /* printf("genre   - \"%s\"\n", taglib_tag_genre(tag)); */
   }
 
   // Store tag data in a database
@@ -105,6 +196,9 @@ int main(int argc, char *argv[])
   char *dir_name = "/media/cpd/1233-621A/Audiobooks/";
   //  scan_dir("/media/cpd/TOSHIBA/Audiobooks/Gregory Benford - Galactic Center - 01 - In The Ocean Of Night", 0);
 //  scan_dir("/media/cpd/TOSHIBA/Audiobooks", 0);
+
+  printf("%s\n", sqlite3_libversion()); 
+
   scan_dir(dir_name, 0);
 
   
