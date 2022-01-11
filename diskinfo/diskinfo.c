dc59a0fd3462d07e70b3056dd14b6814abfb96ca
cpd <ciaran.dunn@gmail.com>
2022-01-11 00:12:46 +0000

    Diskinfo I guess

--- diskinfo/diskinfo.c
+++ diskinfo/diskinfo.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 #include <sys/statvfs.h>
+#include <sys/statfs.h>
 
+void dump_statvfs(struct statvfs *stat)
+{
+  printf("f_bsize     = %ld\n", stat->f_bsize);
+  printf("f_frsize    = %ld\n", stat->f_frsize);
+  printf("f_blocks    = %ld\n", stat->f_blocks);
+  printf("f_bfree     = %ld\n", stat->f_bfree);
+  printf("f_bavail    = %ld\n", stat->f_bavail);
+  printf("f_fsid      = %ld\n", stat->f_fsid);
+  printf("f_flag      = 0x%0lX", stat->f_flag);
+  printf(" (");
+//  if (stat->f_flag && ST_MANDLOCK)
+//  {
+//    printf(" ST_MANDLOCK ");
+//  }
+  printf(")\n");
+  printf("f_namemax   = %ld\n", stat->f_namemax);
+}
 
 long get_disk_space(const char *path)
 {
   struct statvfs stat;
 
   int rc = statvfs(path, &stat);
+  dump_statvfs(&stat);
+
   if (rc == 0)
   {
     printf("f_bsize  = %ld\n", stat.f_bsize);
