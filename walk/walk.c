0c9d05b0f2078ba3f7a1a449922f0aba39636602
cpd <ciaran.dunn@gmail.com>
2022-02-05 20:10:21 +0000

    Add walk dir

---
+++ walk/walk.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+struct walk_result
+{
+
+
+};
+
+
+struct walk_result *walk(const char *path)
+{
+
+
+  return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+
+  return 0;
+}
